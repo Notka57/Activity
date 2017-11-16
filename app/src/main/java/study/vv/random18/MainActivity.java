59fde6e757ff5af4aae8613dc37497d76ea409d6
Notka57 <notkasss57@mail.ru>
2017-11-17 01:29:05 +0300

    SecondRandom

--- app/src/main/java/study/vv/random18/MainActivity.java
+++ app/src/main/java/study/vv/random18/MainActivity.java
@@ -19,12 +19,52 @@ public class MainActivity extends AppCompatActivity {
         String num1text = num1.getText().toString();
         EditText num2 =
                 (EditText) this.findViewById(R.id.num2);
-        String num2text = num1.getText().toString();
+        String num2text = num2.getText().toString();
         TextView out =
                 (TextView) this.findViewById(R.id.out);
         out.setText(""+(Double.parseDouble(num1text) + Double.parseDouble(num2text)));
     }
 
     public void minus(View view) {
+        EditText num1 =
+                (EditText) this.findViewById(R.id.num1);
+        String num1text = num1.getText().toString();
+        EditText num2 =
+                (EditText) this.findViewById(R.id.num2);
+        String num2text = num2.getText().toString();
+        TextView out =
+                (TextView) this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(num1text) - Double.parseDouble(num2text)));
+
+
+    }
+
+
+    public void mult(View view) {
+        EditText num1 =
+                (EditText) this.findViewById(R.id.num1);
+        String num1text = num1.getText().toString();
+        EditText num2 =
+                (EditText) this.findViewById(R.id.num2);
+        String num2text = num2.getText().toString();
+        TextView out =
+                (TextView) this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(num1text) * Double.parseDouble(num2text)));
+
+
+    }
+
+    public void division(View view) {
+        EditText num1 =
+                (EditText) this.findViewById(R.id.num1);
+        String num1text = num1.getText().toString();
+        EditText num2 =
+                (EditText) this.findViewById(R.id.num2);
+        String num2text = num2.getText().toString();
+        TextView out =
+                (TextView) this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(num1text) / Double.parseDouble(num2text)));
+
+
     }
 }
